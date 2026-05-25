7025f245a734e43f7f6b70e44ab434e0fc6f834e
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-25 15:01:10 +0200

    Added loop for multiple email analysis

--- email_classifier.py
+++ email_classifier.py
@@ -3,41 +3,48 @@ import json
 
 client = OpenAI()
 
-customer_email = input("Bitte Kundenmail eingeben: ")
+running = True
 
-response = client.responses.create(
-    model="gpt-4.1-mini",
-    input=f"""
-    Analysiere folgende Kundenemail.
+while running:
+    customer_email = input("Bitte Kundenmail eingeben: ")
 
-    Gib die Antwort ausschließlich als JSON zurück.
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=f"""
+        Analysiere folgende Kundenemail.
 
-    Die JSON soll enthalten:
-    - category
-    - priority
-    - summary
-    - suggested_reply
+        Gib die Antwort ausschließlich als JSON zurück.
 
-    Kundenmail:
-    "{customer_email}"
-    """
-)
+        Die JSON soll enthalten:
+        - category
+        - priority
+        - summary
+        - suggested_reply
 
-clean_output = response.output_text.strip()
+        Kundenmail:
+        "{customer_email}"
+        """
+    )
 
-clean_output = clean_output.removeprefix("```json")
-clean_output = clean_output.removeprefix("```")
-clean_output = clean_output.removesuffix("```")
+    clean_output = response.output_text.strip()
 
-clean_output = clean_output.strip()
+    clean_output = clean_output.removeprefix("```json")
+    clean_output = clean_output.removeprefix("```")
+    clean_output = clean_output.removesuffix("```")
 
-try:
-    data = json.loads(clean_output)
+    clean_output = clean_output.strip()
 
-    print("Kategorie:", data["category"])
-    print("Priorität:", data["priority"])
-    print("Zusammenfassung", data["summary"])
-    print("Antwortvorschlag:", data["suggested_reply"])
+    try:
+        data = json.loads(clean_output)
 
-except json.JSONDecodeError:
-    print("Fehler: Die AI Antwort war kein gültiges JSON.")
+        print("Kategorie:", data["category"])
+        print("Priorität:", data["priority"])
+        print("Zusammenfassung", data["summary"])
+        print("Antwortvorschlag:", data["suggested_reply"])
+
+    except json.JSONDecodeError:
+        print("Fehler: Die AI Antwort war kein gültiges JSON.")
+
+    continue_program = input("Weitere Email analysieren? (ja/nein): ")
+    if continue_program.lower() == "nein":
+        running = False
